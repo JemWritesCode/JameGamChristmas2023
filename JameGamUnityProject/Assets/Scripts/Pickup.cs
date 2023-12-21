6f0707058f9d3378b8a948764e6299cb9e9190bf
Jem Dawson <thejemmeh@gmail.com>
2023-12-20 16:01:08 -0800

    can pickup the item although it yeets the guy

--- JameGamUnityProject/Assets/Scripts/Pickup.cs
+++ JameGamUnityProject/Assets/Scripts/Pickup.cs
@@ -6,14 +6,47 @@ namespace JameGam
 {
     public class Pickup : MonoBehaviour
     {
-        // Update is called once per frame
+        public GameObject myHands; //reference to your hands/the position where you want your object to go
+        bool canpickup; //a bool to see if you can or cant pick up the item
+        GameObject ObjectIwantToPickUp; // the gameobject onwhich you collided with
+        bool hasItem; // a bool to see if you have an item in your hand
+        void Start()
+        {
+            canpickup = false;    //setting both to false
+            hasItem = false;
+        }
+
         void Update()
         {
-            // need to change this to trigger only once
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (canpickup == true) // if you enter thecollider of the objecct
             {
-                Debug.Log("hiya");
+                if (Input.GetKeyDown(KeyCode.Space))  // can be e or any key
+                {
+                    ObjectIwantToPickUp.GetComponent<Rigidbody>().isKinematic = true;   //makes the rigidbody not be acted upon by forces
+                    ObjectIwantToPickUp.transform.position = myHands.transform.position; // sets the position of the object to your hand position
+                    ObjectIwantToPickUp.transform.parent = myHands.transform; //makes the object become a child of the parent so that it moves with the hands
+                }
             }
+            if (Input.GetKeyDown(KeyCode.Space) && hasItem == true) // if you have an item and get the key to remove the object, again can be any key
+            {
+                ObjectIwantToPickUp.GetComponent<Rigidbody>().isKinematic = false; // make the rigidbody work again
+
+                ObjectIwantToPickUp.transform.parent = null; // make the object no be a child of the hands
+            }
+        }
+        private void OnTriggerEnter(Collider other) // to see when the player enters the collider
+        {
+            if (other.gameObject.tag == "object") //on the object you want to pick up set the tag to be anything, in this case "object"
+            {
+                canpickup = true;  //set the pick up bool to true
+                ObjectIwantToPickUp = other.gameObject; //set the gameobject you collided with to one you can reference
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            canpickup = false; //when you leave the collider set the canpickup bool to false
+
         }
     }
 }
+
