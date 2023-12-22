08fa5a2a1fa625b8551468c666b99fccaf87851b
redseiko <redseiko@gmail.com>
2023-12-22 00:36:07 -0700

    Add new TimerPanel with basic TimerPanelController.
    Adjust SettingsPanel sfx and logic.

--- JameGamUnityProject/Assets/UI/Scripts/TimerPanelController.cs
+++ JameGamUnityProject/Assets/UI/Scripts/TimerPanelController.cs
@@ -1,68 +1,69 @@
 using DG.Tweening;
 
+using TMPro;
+
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace JameGam.UI {
-  public sealed class SettingsPanelController : MonoBehaviour {
+  public sealed class TimerPanelController : MonoBehaviour {
     [field: SerializeField, Header("Panel")]
     public RectTransform Panel { get; private set; }
 
     [field: SerializeField]
     public CanvasGroup PanelCanvasGroup { get; private set; }
 
-    [field: SerializeField, Header("SFX")]
-    public AudioSource SfxAudio { get; private set; }
+    [field: SerializeField, Header("Timer")]
+    public TMP_Text TimerLabel { get; private set; }
 
     [field: SerializeField]
-    public AudioClip ShowPanelSfx { get; private set; }
+    public Image TimerIcon { get; private set; }
 
-    [field: SerializeField]
-    public AudioClip HidePanelSfx { get; private set; }
-
-    [field: SerializeField, Header("Audio Volume")]
-    public Slider AudioVolumeSlider { get; private set; }
+    [field: SerializeField, Header("Sfx")]
+    public AudioSource SfxAudioSource { get; private set; }
 
     public bool IsPanelVisible { get; private set; }
+    public float CurrentTimer { get; private set; }
 
     private void Awake() {
       ResetPanel();
+    }
+
+    private void Start() {
+      ShowPanel();
     }
 
     public void ResetPanel() {
       PanelCanvasGroup.alpha = 0f;
       PanelCanvasGroup.blocksRaycasts = false;
       IsPanelVisible = false;
+
+      TimerLabel.text = "-:--";
+      CurrentTimer = 0f;
     }
 
     public void ShowPanel() {
       Panel.DOComplete(withCallbacks: true);
-      IsPanelVisible = true;
 
       DOTween.Sequence()
           .SetTarget(Panel)
           .Insert(0f, PanelCanvasGroup.DOFade(1f, 0.25f))
-          .InsertCallback(0f, () => SfxAudio.PlayOneShot(ShowPanelSfx))
           .OnComplete(() => {
             PanelCanvasGroup.blocksRaycasts = true;
+            IsPanelVisible = true;
           });
     }
 
     public void HidePanel() {
       Panel.DOComplete(withCallbacks: true);
-      IsPanelVisible = false;
 
       DOTween.Sequence()
           .SetTarget(Panel)
           .Insert(0f, PanelCanvasGroup.DOFade(0f, 0.25f))
-          .InsertCallback(0f, () => SfxAudio.PlayOneShot(HidePanelSfx))
           .OnComplete(() => {
             PanelCanvasGroup.blocksRaycasts = false;
+            IsPanelVisible = false;
           });
-    }
-
-    public void OnAudioVolumeSliderChanged(float value) {
-      AudioListener.volume = value;
     }
   }
 }
